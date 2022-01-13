55e0d3283d3aa59de9243787368d2e941808195c
deano98 <deano_98@hotmail.com>
2022-01-13 23:23:11 +0000

    Add test for markets view

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import *
 
-# Create your tests here.
+class TestViews(TestCase):
+
+    def test_markets(self):
+        client = Client()
+
+        response = client.get(reverse('index'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    
--- app/views.py
+++ app/views.py
@@ -5,3 +5,4 @@ def markets(request):
     cg = CoinGeckoAPI()
     all_markets = cg.get_coins_markets(order='market_cap_desc', vs_currency='usd', per_page=10, page=1)
     return render(request, 'index.html', {'all_markets': all_markets})
+
