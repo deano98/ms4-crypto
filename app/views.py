2a8e16446466643b0d6b74c0d9e779786f33af96
deano98 <deano_98@hotmail.com>
2022-01-13 18:58:11 +0000

    Update URLs and add base and index templates to render market view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from pycoingecko import CoinGeckoAPI
 
-# Create your views here.
+def markets(request):
+    cg = CoinGeckoAPI()
+    all_markets = cg.get_coins_markets(order='market_cap_desc', vs_currency='usd', per_page=10, page=1)
+    return render(request, 'index.html', {'all_markets': all_markets})
